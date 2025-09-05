d36fa11deef89ea3d5ced467138d1f80862d027a
korniplodiks <korniplodiks@users.noreply.github.com>
2025-09-05 18:49:35 +0300

    feat: improve code readability

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 35
 module.exports = API;
+
+
+// API module for SpoonRecipe
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 59
+module.exports = API;
