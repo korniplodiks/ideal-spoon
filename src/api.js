05ab6fe87cd5cb96a077c0cd90bfa427d6ec64e8
korniplodiks <korniplodiks@users.noreply.github.com>
2025-09-08 16:13:37 +0300

    remove: update dependencies

--- src/api.js
+++ src/api.js
@@ -131,3 +131,30 @@ class API {
 
 // Update 59
 module.exports = API;
+
+
+// API module for SpoonRecipe
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 60
+module.exports = API;
