49164fde5c4b813e6a96fd0974730790e21526f4
korniplodiks <korniplodiks@users.noreply.github.com>
2025-12-01 03:09:36 +0200

    docs(api): add unit tests

--- src/api.js
+++ src/api.js
@@ -158,3 +158,30 @@ class API {
 
 // Update 60
 module.exports = API;
+
+
+// API module for SpoonRecipe
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 61
+module.exports = API;
