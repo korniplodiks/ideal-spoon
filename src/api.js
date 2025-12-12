c60947137074b53d642549ce5f0131bdcb38bc0c
korniplodiks <korniplodiks@users.noreply.github.com>
2025-12-12 16:18:59 +0200

    add(api): add input validation

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 20
 module.exports = API;
+
+
+// API module for SpoonRecipe
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 35
+module.exports = API;
